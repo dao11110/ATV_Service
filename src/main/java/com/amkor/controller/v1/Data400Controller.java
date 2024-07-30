657d966e36865f303529811e21038018e3f01b6e
thanh.truongcong <thanh.truongcong@amkor.co>
2024-07-30 14:14:12 +0700

    CHG0151720: change: datasource from ehq to prd

--- src/main/java/com/amkor/controller/v1/Data400Controller.java
+++ src/main/java/com/amkor/controller/v1/Data400Controller.java
@@ -40,7 +40,7 @@ public class Data400Controller {
                 result = "jdbc:as400://10.101.6.12";
                 break;
             case "ATV":
-                result = "jdbc:as400://10.201.6.21";
+                result = "jdbc:as400://10.201.6.11";
                 break;
         }
         return result;
